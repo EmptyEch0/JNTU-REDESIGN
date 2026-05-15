5545f3d808ac7c1003484ae22e8f2d8fc83bbbd9
saivamsi611 <sv7195203@gmail.com>
2026-05-15 15:07:38 +0530

    Remove connection string from create_table.ts
    Removed sensitive connection string from create_table.ts.

--- scratch/create_table.ts
+++ scratch/create_table.ts
@@ -1,6 +1,5 @@
 import postgres from 'postgres';
 
-const connectionString = "postgresql://neondb_owner:npg_VumPW7fSI0JO@ep-lingering-mountain-aom9cqy0-pooler.c-2.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require";
 
 async function main() {
   const sql = postgres(connectionString);
